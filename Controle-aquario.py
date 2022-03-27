a4ed27340bdcaa9cfb530d6877a19b18cbcb6e7e
LeandroLDA <45002731+LeandroLDA@users.noreply.github.com>
2022-03-27 20:55:56 -0300

    Primeiro Push

---
+++ Controle-aquario.py
@@ -0,0 +1,14 @@
+'Controle de aquario'
+from operator import truediv
+import time
+import tkinter
+import turtle
+
+
+temp_ext = float (input(""))
+temp_int = float (input(""))
+comida = bool
+vent = bool
+
+if (temp_int >= (temp_ext + 3.0)):
+vent = true
